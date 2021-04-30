a647a273e7e52f0a7836023416d7196aa938afcb
FruitGooseKSP <jrf091981@gmail.com>
2021-04-30 23:50:43 +0100

    save

--- ParachutesLetsUseMaths/GUIElements.cs
+++ ParachutesLetsUseMaths/GUIElements.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
+
 namespace ParachutesLetsUseMaths
 {
     [KSPAddon(KSPAddon.Startup.EveryScene, false)]
@@ -13,6 +14,8 @@ namespace ParachutesLetsUseMaths
 
         public Texture btnTxtOn;
         public Texture btnTxtOff;
+        public PlumUtilities pUtils = new PlumUtilities();
+        public bool chutesOnboard = false;
 
         public static ApplicationLauncherButton plumBtn;
         public static bool btnIsPressed;
@@ -21,6 +24,30 @@ namespace ParachutesLetsUseMaths
         public static Rect guiPos;
         public Vector2 menuPosition;
         public Vector2 menuSize;
+        public static int celPick;
+        public static int calcPick;
+        public static int storedCel;
+        public static int storedCalc;
+        public static string[] bodies =
+        {
+            "Kerbin",
+            "Eve",
+            "Duna",
+            "Laythe",
+        };
+        public static string[] calcs =
+        {
+            "Current Touchdown Velocity",
+            "Parachutes Required For Mass",
+        };
+
+        public GUIStyle styleBtn;
+        public GUIStyle styleLabel;
+        public GUIStyle styleLabel2;
+        public GUIStyle styleLabel3;
+        public GUIStyle styleToggle;
+        public GUIStyle styleToggle2;
+        public GUIStyle styleBox;
 
 
 
@@ -39,19 +66,64 @@ namespace ParachutesLetsUseMaths
                 btnTxtOff = GameDatabase.Instance.GetTexture("FruitKocktail/VertikalSpeedIndicator/Icons/vsioff", false);
                 btnTxtOn = GameDatabase.Instance.GetTexture("FruitKocktail/VertikalSpeedIndicator/Icons/vsioff", false);
 
-                menuSize = new Vector2(800, 600);
+                menuSize = new Vector2(800, 550);
                 menuPosition = new Vector2((Screen.width / 2) - (menuSize.x / 2), (Screen.height / 2 ) - (menuSize.y / 2));
                 guiPos = new Rect(menuPosition, menuSize);
                 btnIsPressed = false;
                 closeBtn = false;
 
                 plumBtn = ApplicationLauncher.Instance.AddModApplication(onTrue, onFalse, onHover, onHoverOut, null, null,
-                    ApplicationLauncher.AppScenes.FLIGHT, btnTxtOff);
+                    ApplicationLauncher.AppScenes.VAB | ApplicationLauncher.AppScenes.SPH, btnTxtOff);
                 
+
                 btnIsPresent = true;
+                celPick = 0;
+                storedCel = 0;
+                calcPick = 0;
+                storedCalc = 0;
+
+                styleBtn = new GUIStyle(HighLogic.Skin.button);
+                styleLabel = new GUIStyle(HighLogic.Skin.label);
+                styleLabel2 = new GUIStyle(HighLogic.Skin.label)
+                {
+                    margin = new RectOffset(50, 50, 25,25),
+                    
+                
+                };
+
+                styleLabel2.normal.textColor = Color.white;
+
+                styleLabel3 = new GUIStyle(HighLogic.Skin.label)
+                {
+                    margin = new RectOffset(50, 50, 25, 25),
+                    fontStyle = FontStyle.Bold,
+                    
+                };
+               
+                
+                styleToggle = new GUIStyle(HighLogic.Skin.toggle)
+                {
+                    
+                    margin = new RectOffset(150, 150, 25, 25),
+                    stretchWidth = true,
+                   
+                };
+
+                styleToggle2 = new GUIStyle(HighLogic.Skin.toggle)
+                {
+
+                    margin = new RectOffset(250, 250, 25, 25),
+                    stretchWidth = true,
+
+                };
+
+                styleBox = new GUIStyle(HighLogic.Skin.box)
+                {
+                    
+                    border = new RectOffset(25, 25, 25, 25),
 
 
-
+                };
 
 
 
@@ -59,7 +131,15 @@ namespace ParachutesLetsUseMaths
 
 
             }
-
+            else
+            {
+                if (plumBtn != null)
+                {
+                    onDestroy();
+                    plumBtn = null;
+                    btnIsPresent = false;
+                }
+            }
 
 
 
@@ -69,23 +149,233 @@ namespace ParachutesLetsUseMaths
 
         public void Update()
         {
+            if (HighLogic.LoadedSceneIsEditor)
+            {
+                if (plumBtn != null)
+                {
+                    if (closeBtn)
+                    {
+                        plumBtn.SetFalse();
+                        closeBtn = false;
+                    }
+
+                    else if (btnIsPresent)
+                    {
+                        
+                    }
+
+
+
+
+                }
+            }
+            else
+            {
+                if (plumBtn != null)
+                {
+                    onDestroy();
+                    plumBtn = null;
+                    btnIsPresent = false;
+                }
+            }
+
+        }
+
+        public string GetParachuteQty()
+        {
+            if (btnIsPressed)
+            {
+
+                int paraCount = 0;
+
+                foreach (var part in EditorLogic.fetch.ship.parts)
+                {
+                    if (part.HasModuleImplementing<ModuleParachute>())
+                    {
+                        part.Highlight(true);
+                        paraCount += 1;
+                    }
+                }
+
+                if (paraCount == 0)
+                {
+                    chutesOnboard = false;
+                    return "No Parachutes Onboard!";
+                }
+                else
+                {
+                    chutesOnboard = true;
+                    return paraCount.ToString();
+                }
+
+            }
+
+            else return null;
+
 
 
         }
 
+        public string GetVesselMass()
+        {
+            if (btnIsPressed)
+            {
+                float vesDM;
+                float vesWM;
+                EditorLogic.fetch.ship.GetShipMass(out vesDM, out vesWM);
+
+                float totalMass = (vesDM + vesWM) * 1000;
+
+                return totalMass.ToString();
+            }
+
+            else return null;
+        }
+
+        public string GetATD()
+        {
+           
+                return pUtils.FetchATD(celPick);
+          
+        }
+
+        public string GetSurfaceGravity()
+        {
+           
+                return pUtils.FetchSG(celPick);
+            
+            
+        }
+
+        public string GetBValue()
+        {
+            if (chutesOnboard)
+            {
+                double runningTotal = 0;
+
+                foreach (var part in EditorLogic.fetch.ship.parts)
+                {
+                    if (part.HasModuleImplementing<ModuleParachute>())
+                    {
+                        string name = part.name;
+                        int paraCode;
+
+                        if (name == "parachuteSingle")
+                        {
+                            paraCode = 0;
+                        }
+                        else if (name == "parachuteRadial")
+                        {
+                            paraCode = 1;
+                        }
+                        else if (name == "parachuteLarge")
+                        {
+                            paraCode = 2;
+                        }
+                        else if (name == "parachuteDrogue")
+                        {
+                            paraCode = 3;
+                        }
+                        else if (name == "radialDrogue")
+                        {
+                            paraCode = 4;
+                        }
+                        else if (name == "tantares_parachute_s0_1")
+                        {
+                            paraCode = 0;
+                        }
+                        else paraCode = 0;
+
+                        double tempVal = pUtils.BCodeBase(celPick, paraCode);
+
+                        runningTotal += tempVal;
+
+
+                    }
+
+
+                }
+
+                return runningTotal.ToString();
+            }
+            else return "";
+
+        }
+
+
+        public string GetTDVelocity()
+        {
+            if (chutesOnboard)
+            {
+                double m = Double.Parse(GetVesselMass());
+                double b = Double.Parse(GetBValue());
+                double p = Double.Parse(GetParachuteQty());
+
+                double touchDownVelocity = Math.Sqrt(m * (b / p));
+
+                if (touchDownVelocity > 8)
+                {
+                    styleLabel3.normal.textColor = Color.red;
+                }
+                else
+                {
+                    styleLabel3.normal.textColor = Color.green;
+                }
+
+                return touchDownVelocity.ToString();
+            }
+            else
+            {
+                styleLabel3.normal.textColor = Color.grey;
+                return "";
+            }
+        }
+
+
+
         public void MenuWindow(int WindowID)
         {
+            GUI.BeginGroup(new Rect(0, 0, menuSize.x, menuSize.y));
+            GUI.Box(new Rect(0, 0, menuSize.x, menuSize.y), GUIContent.none);
 
+            
 
+            GUI.Label(new Rect(50, 35, menuSize.x - 100, 25), "Select Celestial Body:", styleLabel);
 
+            celPick = GUI.SelectionGrid(new Rect(50, 70, menuSize.x - 100, 25), celPick, bodies, 4, styleToggle);
 
+            GUI.Label(new Rect(50, 125, menuSize.x - 100, 25), "Choose Calculation Type:", styleLabel);
+
+            calcPick = GUI.SelectionGrid(new Rect(50, 170, menuSize.x - 100, 25), calcPick, calcs, 2, styleToggle2);
+
+            GUI.Label(new Rect(50, 225, menuSize.x - 100, 25), "Calculations", styleLabel);
+
+            GUI.Box(new Rect(50, 250, menuSize.x - 100, 200), GUIContent.none, styleBox);
+
+            GUI.Label(new Rect(75, 275, menuSize.x- 150, 25), "Parachute Quantity = " + GetParachuteQty(), styleLabel2);
+
+            GUI.Label(new Rect(75, 300, menuSize.x - 150, 25), "Vessel (Wet) Mass, Kg = " + GetVesselMass(), styleLabel2);
+
+            GUI.Label(new Rect(75, 325, menuSize.x - 150, 25), "Atomospheric Density, kPa = " + GetATD(), styleLabel2);
+
+            GUI.Label(new Rect(75, 350, menuSize.x - 150, 25), "Surface Gravity, m/s2 = " + GetSurfaceGravity(), styleLabel2);
+
+            GUI.Label(new Rect(75, 375, menuSize.x - 150, 25), "Combined Parachute Magnitude Factor, bPmf = " + GetBValue(), styleLabel2);
+
+            GUI.Label(new Rect(75, 400, menuSize.x - 150, 25), "Predicted Touchdown Velocity, m/s = " + GetTDVelocity(), styleLabel3);
+
+            closeBtn = GUI.Button(new Rect(menuSize.x - 150, menuSize.y - 75, 100, 50), "Close", styleBtn);
+
+            GUI.DragWindow();
+
+            GUI.EndGroup();
 
         }
 
         public void ItsPlumTime()
         {
             guiPos = GUI.Window(123458, guiPos, MenuWindow,
-               "", new GUIStyle(HighLogic.Skin.window));
+               "Parachute Information", new GUIStyle(HighLogic.Skin.window));
 
 
 
@@ -126,6 +416,14 @@ namespace ParachutesLetsUseMaths
             {
                 plumBtn.SetTexture(btnTxtOff);
                 btnIsPressed = false;
+                
+                foreach (var part in EditorLogic.fetch.ship.parts)
+                {
+                    if (part.HighlightActive)
+                    {
+                        part.Highlight(false);
+                    }
+                }
             }
         }
 
--- ParachutesLetsUseMaths/PlumUtilities.cs
+++ ParachutesLetsUseMaths/PlumUtilities.cs
@@ -8,8 +8,150 @@ namespace ParachutesLetsUseMaths
 {
     public class PlumUtilities
     {
+        private Dictionary<int, double> bValsK = new Dictionary<int, double>();
+        private Dictionary<int, double> bValsE = new Dictionary<int, double>();
+        private Dictionary<int, double> bValsD = new Dictionary<int, double>();
+        private Dictionary<int, double> bValsL = new Dictionary<int, double>();
 
 
+        public PlumUtilities()
+        {
+            CreateDictionaryOfBValues();
+
+
+        }
+
+        private void CreateDictionaryOfBValues()
+        {
+
+            const double k0 = 0.029;
+            const double k1 = 0.039;
+            const double k2 = 0.020;
+            const double k3 = 1.099;
+            const double k4 = 1.065;
+
+            const double e0 = 0.009;
+            const double e1 = 0.012;
+            const double e2 = 0.006;
+            const double e3 = 0.340;
+            const double e4 = 0.330;
+
+            const double d0 = 0.079;
+            const double d1 = 0.105;
+            const double d2 = 0.054;
+            const double d3 = 2.967;
+            const double d4 = 2.877;
+
+            const double l0 = 0.036;
+            const double l1 = 0.047;
+            const double l2 = 0.024;
+            const double l3 = 1.331;
+            const double l4 = 1.291;
+
+            bValsK.Add(0, k0);
+            bValsK.Add(1, k1);
+            bValsK.Add(2, k2);
+            bValsK.Add(3, k3);
+            bValsK.Add(4, k4);
+
+            bValsE.Add(0, e0);
+            bValsE.Add(1, e1);
+            bValsE.Add(2, e2);
+            bValsE.Add(3, e3);
+            bValsE.Add(4, e4);
+
+            bValsD.Add(0, d0);
+            bValsD.Add(1, d1);
+            bValsD.Add(2, d2);
+            bValsD.Add(3, d3);
+            bValsD.Add(4, d4);
+
+            bValsL.Add(0, l0);
+            bValsL.Add(1, l1);
+            bValsL.Add(2, l2);
+            bValsL.Add(3, l3);
+            bValsL.Add(4, l4);
+
+
+        }
+
+        public double BCodeBase(int planet, int chute)
+        {
+
+            switch (planet)
+            {
+                case 0:
+                    return bValsK[chute];
+                case 1:
+                    return bValsE[chute];
+                case 2:
+                    return bValsD[chute];
+                case 3:
+                    return bValsL[chute];
+                default:
+                    return bValsK[chute];
+            }
+
+
+
+        }
+
+        public string FetchATD(int selCode)
+        {
+            string bodyStr;
+
+            switch (selCode)
+            {
+                case 0:
+                    bodyStr = "101.325";
+                    break;
+                case 1:
+                    bodyStr = "506.625";
+                    break;
+                case 2:
+                    bodyStr = "6.75500";
+                    break;
+                case 3:
+                    bodyStr = "60.7950";
+                    break;
+                default:
+                    bodyStr = "101.325";
+                    break;
+            }
+
+            return bodyStr;
+
+        }
+
+        public string FetchSG(int selCode)
+        {
+            string bodyStr;
+
+            switch (selCode)
+            {
+                case 0:
+                    bodyStr = "9.81";
+                    break;
+                case 1:
+                    bodyStr = "16.7";
+                    break;
+                case 2:
+                    bodyStr = "2.94";
+                    break;
+                case 3:
+                    bodyStr = "7.85";
+                    break;
+                default:
+                    bodyStr = "9.81";
+                    break;
+            }
+
+            return bodyStr;
+
+
+        }
+
+        
 
 
     }
